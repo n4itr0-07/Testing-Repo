7c35565ddaec8042d7b7bf10c58a2fe976dcf500
N4!TRO 07 <168121164+n4itr0-07@users.noreply.github.com>
2024-11-21 22:16:24 +0530

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,15 @@
+-- main.lua
+
+-- Function to print a greeting message
+local function greet(name)
+    print("Hello, " .. name .. "!")
+end
+
+-- Call the greet function
+greet("World")
+
+-- Function to print a farewell message
+local function farewell(name)
+    print("Goodbye, " .. name .. "!")
+end
+
